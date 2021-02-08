3eb79bd4ec44e206b7e8801e2b17291764688996
Nishat Hoque <hoquen0397@gmail.com>
2021-02-08 12:28:30 -0500

    power calculator

--- practice.js
+++ practice.js
@@ -12,3 +12,20 @@ function countingSheep(num){
 }
 
 countingSheep(3)
+
+/****************************Power Calculator***************************/
+function powerCalculator(base, exp){
+    if(exp < 0){
+        return console.log( 'exponent should be >= 0')
+    }
+    if(exp === 0){
+        return 1
+    }
+    if(exp === 1){
+        return base
+    }
+
+    return base * powerCalculator(base, exp-1)
+}
+
+console.log(powerCalculator(10, 1))
